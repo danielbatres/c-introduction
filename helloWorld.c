ac27be35d5c26c8659782087178694180be97ac3
danielbatres <danielbatresuser@gmail.com>
2023-11-19 21:21:48 -0600

    Variable declaration

--- helloWorld.c
+++ helloWorld.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+int i, j, k;
+unsigned int minimumMinutes = 30;
+
+char y = 'y';
+unsigned char dailyWorkedHours = 0;
+
 int main() {
   printf("Hello world");
   
---
+++ variables.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// Variable declarations
+
+int a, b, c;
+
+float f, g, h;
+
+int main() {
+  // Variable initialization
+  
+  a = 1;
+  b = 34;
+  
+  c = a + b;
+  
+  printf("Sum value is: %d", c);
+  
+  return 0;
+}
