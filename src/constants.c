13975d467b9240f64c2e9f8ac0f1214aefd1afb8
danielbatres <danielbatresuser@gmail.com>
2023-11-20 12:16:59 -0600

    Preprocessor directives: Define

--- src/constants.c
+++ src/constants.c
@@ -1,3 +1,18 @@
-const int hogdogPrice = 100;
-const float totalBill = 100.58;
-const char newLine = '\n';
+#include <stdio.h>
+
+const int HOGDOGPRICE = 100;
+const float BILL = 100.58;
+const char NEWLINE = '\n';
+
+#define PIZZACOST 1.5
+
+int main() {
+  float pizzaCost;
+  float slicesNumber = 3;
+
+  pizzaCost = PIZZACOST * slicesNumber;
+
+  printf("Total bill:%c %f", NEWLINE, pizzaCost);
+
+  return 0;
+}
