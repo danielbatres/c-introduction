6782986b6d0ab360dc3a45a422009a7265673955
danielbatres <danielbatresuser@gmail.com>
2023-11-19 20:31:23 -0600

    Data types

---
+++ data-types.c
@@ -0,0 +1,22 @@
+/*
+*
+* Basic types
+*
+* (a) Arithmetic
+* 
+* int 2 or 4 bytes -32,768 to 32,767 or -2,147,483,648 to 2,147,483,647
+* unsigned int 0 to 65,535 or 0 to 4,294,967,295
+* 
+* short 2 bytes
+* unsigned short 2 bytes
+*
+* long 8 bytes -9223372036854775808 to 9223372036854775807
+* unsigned long 8 bytes 0 to 18446744073709551615
+* 
+* (b) Floating point
+*
+* float 4 bytes 1.2E-38 to 3.4E+38 6 decimal places
+* double 8 bytes 2.3E-308 to 1.7E+30815 (15 decimal places)
+* long double 10 bytes 3.4E-4932 to 1.1E+493219 (19 decimal places)
+*
+*/
